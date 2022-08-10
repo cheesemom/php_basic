ab09184ef93991d8089fc73cc68830198090a087
cheesemom <smsm2610@naver.com>
2022-08-10 14:42:11 +0900

    0810

---
+++ project/assets.php
@@ -0,0 +1,12 @@
+<?
+$title = '현대엘리베이터 - 실습01';
+$company = '현대엘리베이터';
+$tel = '1577-0603';
+$email = 'mimic@naver.com';
+$c_num='607-09-98424';
+$c_ceo='전상미';
+$c_address='부산시 해운대구';
+$c_slogan='현대엘리베이터가 만든 새로운 길을 따라 세상을 위로 넓어집니다.';
+$e_slogan='The world expands along the new path created by Hyundai Elevator';
+?>
+
---
+++ project/index.php
@@ -0,0 +1,55 @@
+<? include 'head.sub.php';?>
+
+<main class="Main">
+    <section class="MainVisual">
+        <div class="MainSlider">
+            <figure class="itm01"></figure>
+            <figure class="itm02"></figure>
+        </div>
+        <div class="slogan">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia enim quibusdam nostrum?</p>
+        </div>
+    </section>
+    <section class="Customer">
+        <ul class="container">
+            <li>
+                <ul>
+                    <li>
+                        <a href="">Lorem ipsum dolor sit amet. <span>2022.12.25</span></a>
+                    </li>
+                    <li>
+                        <a href="">Lorem ipsum dolor sit amet. <span>2022.12.25</span></a>
+                    </li>
+                    <li>
+                        <a href="">Lorem ipsum dolor sit amet. <span>2022.12.25</span></a>
+                    </li>
+                    <li>
+                        <a href="">Lorem ipsum dolor sit amet. <span>2022.12.25</span></a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <div class="CuSlider">
+                <figure>
+                        <img src="./img/product01.jpg" alt="">
+                    </figure>
+                    <figure>
+                        <img src="./img/product02.jpg" alt="">
+                    </figure>
+                    <figure>
+                        <img src="./img/product03.jpg" alt="">
+                    </figure>
+                </div>
+            </li>
+            <li>
+                <div class="contact">
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
+            </li>
+        </ul>
+    </section>
+</main>
+
+<? include 'tail.sub.php';?>
